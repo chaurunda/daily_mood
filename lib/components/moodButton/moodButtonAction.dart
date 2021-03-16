36b392237d1c611ab29190851ddb5cbef063ff5e
Arnaud Huc <arnaud.huc.pro@gmail.com>
2021-03-16 19:27:14 +0100

    Use singlequote

--- lib/components/moodButton/moodButtonAction.dart
+++ lib/components/moodButton/moodButtonAction.dart
@@ -6,14 +6,14 @@ import 'package:intl/intl.dart';
 class MoodButtonAction {
   Future add(int value) {
     var dt = DateTime.now();
-    var newFormat = DateFormat("dd/MM/yyyy");
+    var newFormat = DateFormat('dd/MM/yyyy');
     String updatedDt = newFormat.format(dt);
     print(updatedDt);
-    var url = Uri.parse("http://6f5811d4fcc6.ngrok.io/api");
+    var url = Uri.parse('http://6f5811d4fcc6.ngrok.io/api');
     print(url);
     print({'date': updatedDt, 'value': value});
     return http.post(url,
-        headers: {"Content-Type": "application/json"},
+        headers: {'Content-Type': 'application/json'},
         body: jsonEncode({'date': '$updatedDt', 'value': value}));
   }
 }
