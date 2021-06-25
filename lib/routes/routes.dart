88495f897d1105b5df8cdecc40dd558ecd498809
Arnaud Huc <arnaud.huc.pro@gmail.com>
2021-06-25 19:57:12 +0200

    Clean all

--- lib/routes/routes.dart
+++
@@ -1,6 +0,0 @@
-import 'package:daily_mood/screens/summary.dart';
-import 'package:flutter/widgets.dart';
-
-final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
-  "/summary": (BuildContext context) => Summary(title: "Recapitulatif"),
-};
