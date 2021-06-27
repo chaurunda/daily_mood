4e1407c2aa2d0dc15f6740fc2daff842e003694e
Arnaud Huc <arnaud.huc.pro@gmail.com>
2021-06-27 11:30:47 +0200

    Finish refactor

---
+++ lib/screens/summary.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Summary extends StatefulWidget {
+  const Summary({Key? key}) : super(key: key);
+
+  @override
+  _SummaryState createState() => _SummaryState();
+}
+
+class _SummaryState extends State<Summary> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to first screen when tapped.
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
